03b9b5aca83e2fe283b52a157cb2868740f7997e
Leo Torres <leo.tn03@icloud.com>
2023-09-30 19:44:11 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Leo Torres Neyra on 30/9/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
